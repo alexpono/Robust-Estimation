021689b9dcab565a97d0f9fadb78a45c034c8ff7
unknown <alexpono@gmail.com>
2021-04-02 11:32:35 +0200

    partial success with tform

--- matching_test_beta.m
+++ matching_test_beta.m
@@ -15,6 +15,9 @@
 
 % list of functions 
 
+close all
+clean all
+
 totalnFrames = 500;
 him = 1152;
 wim = 1152;
@@ -65,7 +68,7 @@ hcam02 = figure;
 imagesc(20*ACC2)
 title('Camera2')
 
-%%  ROBUST ESTIMATION PART 1.3 normxcorr2 smaller and smaller windows
+%%  ROBUST ESTIMATION PART 1.3 normxcorr2 pass 01 (on a large window)
 % xm,ym : fixed points in camera 1
 c = clock; fprintf('start at %0.2dh%0.2dm\n',c(4),c(5)) 
 filterOrder = 10;
@@ -82,36 +85,42 @@ drawrectangle(gca,'Position',[xm-wsub,ym-wsub,2*wsub,2*wsub], ...
 figure(hcam02), hold on
 drawrectangle(gca,'Position',[xoffSet-wsub,yoffSet-wsub,2*wsub,2*wsub], ...
         'FaceAlpha',0,'Color','r');
-dxPass01 =   xoffSet-xm
-dyPass01 =   yoffSet-ym
-R = (dxPass01^2+dyPass01^2)^(1/2) 
+dxPass01 =   xoffSet-xm;
+dyPass01 =   yoffSet-ym;
+R = (dxPass01^2+dyPass01^2)^(1/2);
 c = clock; fprintf('finished at %0.2dh%0.2dm\n',c(4),c(5)) 
 
-%% second pass
+%%  ROBUST ESTIMATION PART 1.3 normxcorr2 pass 02 (on a small window)
+
+wti = 400; % width template images
+wstep = 100; % step for sampling the image
+nPartMin = 50; % minimum number of particles to calculate the correlation
+tmpl_IM_tStr = struct(); % structure storing information on template images
 
 % cut the image in a lot of small images
 hcam01 = figure;
 imagesc(20*ACC1)%, colormap gray
 title('Camera1'), hold on
-wti = 300; % width template images
-tmpl_IM_tStr = struct(); % structure storing information on template images
 clear nCol nRow
-nCol = wim / wti;
-nLin = him / wti;
+nCol = wim / wstep;
+nLin = him / wstep;
 iti = 0;
 for iCol = 1 : nCol
     for iLin = 1 : nLin
-        iti = iti + 1;
         clear xc yc
-        xc = round(1 + round(wti/2) + (iCol-1)*wti*nCol/floor(nCol));
-        yc = round(1 + round(wti/2) + (iLin-1)*wti*nLin/floor(nLin));
+        xc = round(1 + round(wstep/2) + (iCol-1)*wstep*nCol/floor(nCol));
+        yc = round(1 + round(wstep/2) + (iLin-1)*wstep*nLin/floor(nLin));
+        if xc-wti/2 < 0 || yc-wti/2 < 0 || xc+wti/2 > wim || yc+wti/2 > him
+            continue
+        end
+        iti = iti + 1;
         tmpl_IM_tStr(iti).x = xc;
         tmpl_IM_tStr(iti).y = yc;
         clear subIm
         subIm =  ACC1(yc-wti/2:yc+wti/2,xc-wti/2:xc+wti/2);
         tmpl_IM_tStr(iti).subIm = subIm;
         tmpl_IM_tStr(iti).meanSubIm = mean(subIm(:));
-        if tmpl_IM_tStr(iti).meanSubIm*(101*101)/255 > 25  && ...
+        if tmpl_IM_tStr(iti).meanSubIm*(101*101)/255 > nPartMin  && ...
                 (1.5*dxPass01) + xc + wti/2 < wim && ...
                 (1.5*dyPass01) + yc + wti/2 > 0
             tmpl_IM_tStr(iti).correlable = 1;
@@ -124,234 +133,97 @@ for iCol = 1 : nCol
         clear xp yp
         xp = .5*[-1  1  1 -1 -1]*wti+tmpl_IM_tStr(iti).x;
         yp = .5*[-1 -1  1  1 -1]*wti+tmpl_IM_tStr(iti).y;
-        patch('xdata',xp,'ydata',yp,'faceColor',pcol,'faceAlpha',.3,'edgeColor','none')
+        %patch('xdata',xp,'ydata',yp,'faceColor',pcol,'faceAlpha',.3,'edgeColor','none')
         %pause(.2)
         
         if tmpl_IM_tStr(iti).correlable == 1
     clear xm ym xoffSet yoffSet
     xm = tmpl_IM_tStr(iti).x;
     ym = tmpl_IM_tStr(iti).y;
-    [xoffSet,yoffSet] = imageCorrelation(xm,ym,ACC1,ACC2,round(wti/2),filterOrder);
+    [xoffSet,yoffSet] = imageCorrelation(xm,ym,ACC1,ACC2,round(wti/2),filterOrder);%,'cleanC',dxPass01,dyPass01,R);
     tmpl_IM_tStr(iti).xoffSet = xoffSet;
     tmpl_IM_tStr(iti).yoffSet = yoffSet;
-    quiver(xm,ym,xoffSet-xm,yoffSet-ym,'b','lineWidth',5)
+    quiver(xm,ym,xoffSet-xm,yoffSet-ym,'r','lineWidth',1)
         end
     end
 end
 
 
-%% ROBUST ESTIMATION PART 01 -  normxcorr2 - we try to match CC1sub in CC2
-%w = 100; % width correlating zone
-c = clock; fprintf('start at %0.2dh%0.2dm\n',c(4),c(5)) 
+%% ROBUST ESTIMATION PART 1.4 build tform1
+%fixedPoints = [163 427 963 951; 570 781 322 738]';
+%movingPoints = [271 522 1002 1002; 456 631 198 573]';
+clear fixedPoints movingPoints
+corOK = ([tmpl_IM_tStr.correlable] == 1);
+fixedPoints  = [[tmpl_IM_tStr(corOK).x]',      [tmpl_IM_tStr(corOK).y]'];
+movingPoints = [[tmpl_IM_tStr(corOK).xoffSet]',[tmpl_IM_tStr(corOK).yoffSet]'];
+
+% figure
+% clear X Y U V
+% X = fixedPoints(:,1);
+% Y = fixedPoints(:,2);
+% U = movingPoints(:,1)-X;
+% V = movingPoints(:,2)-Y;
+% quiver(X,Y,U,V)
+% set(gca,'ydir','reverse')
 
 transformationType = 'affine';
+tform1 = fitgeotrans(movingPoints,fixedPoints,transformationType);
 
-clear xMP yMP % moving points
-xMP = []; yMP = [];
-
-figure
-imshow(20*ACC1)
-hold on
-for i = 1:length(xm)
-    xc = xm(i);
-    yc = ym(i);
-    drawrectangle(gca,'Position',[xm(i)-w,ym(i)-w,2*w,2*w], ...
-        'FaceAlpha',0,'Color','b');
-end
-title('Selected rectangles from Cam1')
-
-clear xc yc 
-hcam02 = figure;
-imshow(20*ACC2)
-hold on, box on
-%     drawrectangle(gca,'Position',[xoffSet,yoffSet,size(ACC1sub,2),size(ACC1sub,1)], ...
-%         'FaceAlpha',0,'Color','r');
-%     title('Correlated rectangles from Cam1 in Cam2')
-%     hold on
-
-for i = 1 : length(xm)
-    
-    drawrectangle(gca,'Position',[xoffSet,yoffSet,size(ACC1sub,2),size(ACC1sub,1)], ...
-        'FaceAlpha',0,'Color','r');
-    xMP = [xMP,xoffSet+w];
-    yMP = [yMP,yoffSet+w];
-end
-c = clock; fprintf('done at %0.2dh%0.2dm\n',c(4),c(5)) 
-%%
-clear d dx dy
-for ip = 1 : length(xMP)
-    d(ip)  = ((xm(ip)-xMP(ip))^2 + (ym(ip)-yMP(ip))^2)^(1/2);
-    dx(ip) = -xm(ip)+xMP(ip);
-    dy(ip) = -ym(ip)+yMP(ip);
-end
-figure('defaultAxesFontSize',20);
-histogram(d,[0:50:1000])
-title('d')
-figure('defaultAxesFontSize',20);
-histogram(dx)%,[0:50:1000])
-title('dx')
-figure('defaultAxesFontSize',20);
-histogram(dy)%,[0:50:1000])
-title('dy')
-%% plot hitogram of x and y shift
-hhist = figure('defaultAxesFontSize',20);
-set(gca,'ydir','reverse')
-hold on, box on
-for i = 1 : length(xm)
-    plot([xm(i)],[ym(i)],'o')
-    plot([xm(i),xMP(i)],[ym(i),yMP(i)])
-end
-axis([1 1152 1 1152])
-hquiver = figure('defaultAxesFontSize',20);
-imshow(20*ACC1)
-set(gca,'ydir','reverse')
-hold on, box on
-quiver(xm,ym,xMP-xm,yMP-ym)
-axis([1 1152 1 1152])
-%% quiver
-xm = [tmpl_IM_tStr.x];
-ym = [tmpl_IM_tStr.y];
-xMP = [tmpl_IM_tStr.xoffSet];
-xMP = [tmpl_IM_tStr.yoffSet];
-hquiver = figure('defaultAxesFontSize',20);
-imshow(20*ACC1)
-set(gca,'ydir','reverse')
-hold on, box on
-quiver(xm,ym,xMP-xm,yMP-ym)
-axis([1 1152 1 1152])
-
-%%
-
-tform = fitgeotrans(double(fixedPoints),double(movingPoints),transformationType);
-ACC2_transformed = imwarp(ACC2,tform,'OutputView',imref2d(size(ACC1)));
-%%
-figure
-imshowpair(ACC1,ACC2_transformed)
-%imshow(ACC2_transformed)
-
-%% test
-fixedPoints = [100 200 400 100 ; 200 400 300 400]';
-movingPoints = [200 300 500 200 ; 200 400 300 400]';
-
-tform = fitgeotrans(double(movingPoints),double(fixedPoints),transformationType);
-ACC2_transformed = imwarp(ACC2,tform,'OutputView',imref2d(size(ACC1)));
+[X,Y] = transformPointsForward(tform1,271,456);  % check some points
+[X,Y] = transformPointsForward(tform1,0,0);           % check the change of (x0,y0)
+ACC1tformed = imwarp(ACC2,tform1, 'OutputView', imref2d( size(ACC1) ));
 
 falseColorOverlay = imfuse( 40*ACC1, 40*ACC1tformed);
 imshow( falseColorOverlay, 'initialMagnification', 'fit');
 
-
-%%
-
-figure
-imshow(20*ACC2)
-hold on
-for i = 1:length(xm)
-drawrectangle(gca,'Position',[xm(i)-w,ym(i)-w,2*w,2*w], ...
-    'FaceAlpha',0,'Color','b');
-end
-title('Selected rectangles from Cam1')
-
-%%
-fixedPoints = [163 427 963 951; 570 781 322 738]';
-movingPoints = [271 522 1002 1002; 456 631 198 573]';
-
-transformationType = 'affine';
-tform = fitgeotrans(movingPoints,fixedPoints,transformationType);
-
-[X,Y] = transformPointsForward(tform,271,456);  % check some points
-[X,Y] = transformPointsForward(tform,0,0);           % check the change of (x0,y0)
-ACC1tformed = imwarp(ACC2,tform, 'OutputView', imref2d( size(ACC1) ));
-
-falseColorOverlay = imfuse( 40*ACC1, 40*ACC1tformed);
-imshow( falseColorOverlay, 'initialMagnification', 'fit');
-
-%%
-figure, hold on, box on
-for it = 1 : 500
-    %pause(.1)
-    inputPoints = [CC2(it).X;CC2(it).Y]';
-    PointsC1 = [CC1(it).X;CC1(it).Y]';
-    [X,Y] = transformPointsForward(tform,inputPoints(:,1),inputPoints(:,2));
-    
-    plot(X,Y,'or')
-    plot(PointsC1(:,1),PointsC1(:,2),'ob')
-    pause(.1)
-end
-%% OLD Stuff %%
-
-
-%% ROBUST ESTIMATION PART 1.3 - OLD - ginput select the squares for matching 
-w = 75;
-figure
-imshow(imgaussfilt(255-20*ACC1,1))
-
-% matching coordinates
-xm = [];
-ym = [];
-while(1)
-    clear x y
-    [x,y] = ginput(1);
-    if x<0
-        break
-    end
-    xm = [xm,round(x)];
-    ym = [ym,round(y)];
-    drawrectangle(gca,'Position',[x-w,y-w,2*w,2*w], ...
-            'FaceAlpha',0,'Color','b');
-end
-
-
-%%
-% %% a code that could replace ginput, it finds potential correlation zones by itself
-% close all 
-% clear x4corr y4corr
-% h0 = figure;
-% imagesc(50*ACC1), colormap gray
-% 
-% smoothFactor = 8;
-% h1 = figure;
-% ACC1gaussed = imgaussfilt(ACC1,smoothFactor);
-% imagesc(ACC1gaussed)
-% Localmax = imregionalmax(ACC1gaussed,8);
-% h2 = figure;
-% imagesc(Localmax)
-% stats = regionprops(Localmax,'centroid','Area');
-% % h3 = figure;
-% % histogram([stats.Area],[0:50:5000])
-% 
-% hold on
-% icz = 0; % cz for correlation zone
-% for is = 1 : size(stats,1)
-%     if stats(is).Area > 250 || ACC1gaussed(round(stats(is).Centroid(2)),round(stats(is).Centroid(1))) > 1
-%         icz = icz + 1;
-%         x4corr(icz) = stats(is).Centroid(1);
-%         y4corr(icz) = stats(is).Centroid(2);
-%     end
+% figure to check the tform1
+% figure, hold on, box on
+% for it = 1 : 500
+%     %pause(.1)
+%     inputPoints = [CC2(it).X;CC2(it).Y]';
+%     PointsC1 = [CC1(it).X;CC1(it).Y]';
+%     [X,Y] = transformPointsForward(tform,inputPoints(:,1),inputPoints(:,2));
+%     
+%     plot(X,Y,'or')
+%     plot(PointsC1(:,1),PointsC1(:,2),'ob')
+%     pause(.1)
+%     axis([110 210 700 900])
 % end
-% 
-% figure(h0), hold on
-% plot(x4corr,y4corr,'s')
-% figure(h1), hold on
-% plot(x4corr,y4corr,'sr')
-% figure(h2), hold on
-% plot(x4corr,y4corr,'s')
 
+%% ROBUST ESTIMATION PART 2.1 track particles in 2D on each camera
+
+
+
+
+%% testing the function
+imageCorrelation(xm,ym,ACC1,ACC2,round(wti/2),filterOrder,'cleanC',dxPass01,dyPass01,R)
 %% functions
+% debug the cleaning of C (varargin in function)
 
-function [xoffSet,yoffSet] = imageCorrelation(xc,yc,ACC1,ACC2,w,filterOrder)
+function [xoffSet,yoffSet] = imageCorrelation(xc,yc,ACC1,ACC2,w,filterOrder,varargin)
+% varargin: ,'cleanC',dxPass01,dyPass01,R);
     ACC1sub = zeros(w+1,w+1,'uint8');
     ACC1sub = ACC1(yc-w:yc+w,xc-w:xc+w);
     C = normxcorr2(imgaussfilt(ACC1sub,filterOrder),imgaussfilt(ACC2,filterOrder));
     
 
-    % set C to zero above a predefined distance
-    R = 300;
-    x0 = xc+w;
-    y0 = yc+w;
-    x = 1:1302;
-    y = 1:1302;
-    [xx,yy] = meshgrid(x,y);
-    C(((xx-x0).^2+(yy-y0).^2) > R^2)=0;
+    % set C to zero above a predefined radius
+    % Checking varargin structure
+    if ( length(varargin) > 1 )
+        %varargin = varargin{:,2}
+        varargin{:,1}
+        varargin{:,2}
+        varargin{:,3}
+        varargin{:,4}
+        R = varargin{4};      % in pixels
+        x0 = xc+varargin{2};
+        y0 = yc+varargin{3};
+        x = 1:1302;
+        y = 1:1302;
+        [xx,yy] = meshgrid(x,y);
+        C(((xx-x0).^2+(yy-y0).^2) > R^2)=0;
+    end
+
     %
     [ypeak,xpeak] = find(C==max(C(:)));
     yoffSet = ypeak-size(ACC1sub,1) + w;
